8b588bbc04f3dc3900537affaa3527bbc1e37f6b
karl <karl.statz@gmail.com>
2018-12-18 19:59:22 -0600

    adding more dependencies, configuring hasher

--- erlig/src/generator.erl
+++ erlig/src/generator.erl
@@ -1,1 +1,13 @@
 -module(generator).
+-export([generate/0]).
+-record(gen_result, {
+                     time_stamp,
+                     time_stamp_str,
+                     hash
+                    }).
+generate() ->
+    Stamp = os:system_time(),
+    StampStr = lists:flatten(io_lib:format("~p", [Stamp])),
+    <<Hash:64/integer, _/binary>> = murmur:murmur3_x64_128(<<Stamp>>),
+    #gen_result{time_stamp = Stamp, time_stamp_str = StampStr, hash = Hash}.
+   
