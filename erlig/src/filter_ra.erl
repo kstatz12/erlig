24bbc61bfabe036fe0bd5f9fc75ff786480e0447
karl <karl.statz@gmail.com>
2018-12-28 23:01:51 -0600

    adding startup bits

--- erlig/src/filter_ra.erl
+++ erlig/src/filter_ra.erl
@@ -1,24 +1,54 @@
 -module(filter_ra).
--behavior(ra_machine).
--export([init/1, apply/4]).
+-behaviour(ra_machine).
+-export([init/1, apply/3]).
+-export([start/0, start/1, start/2]).
+-export([add/2, check/2]).
 
-%%probably restrict this to a sbf type or something
--opaque state() :: term().
+%% ra types
+-opaque state() :: {term()}.
 
 -type erlig_command() :: {add, Key :: term()} | {check, Key :: term()}.
 
-init(Config) ->
-    Val = maps:find(init_size, Config),
-    sbf:sbf(Val).
-
-apply(_Meta, {add, Key}, Effects, State) ->
-    sbf:add(Key, State),
-    {State, ok, Effects};
-apply(_Meta, {check, Key}, Effects, State) ->
-    Reply = sbf:member(Key, State),
-    {State, Reply, Effects}.
-
+-export_type([
+              state/0,
+              erlig_command/0
+             ]).
+%% start with default name and one node
+start() ->
+    start(filter_ra).
     
+%% start with custom name and one node
+start(Name) ->
+    start(Name, [node()]).
+
+%% start with name and provided nodes
+start(Name, Nodes) when is_list(Nodes) ->
+    Servers = [{Name, N} || N <- Nodes],
+    ra:start_cluster(Name, {module, ?MODULE, #{}}, Servers).
+%public
+add(Server, Key) ->
+    case ra:process_command(Server, {add, Key}) of
+        {ok, _, _} -> ok;
+        Err -> Err
+    end.
+
+check(Server, Key) ->
+    case ra:process_command(Server, {check, Key}) of
+        {ok, Value, _} -> {ok, Value};
+        Err -> Err
+    end.
+
+%%ra_machine callbacks
+init(_) ->
+    sbf:sbf(100000).
+
+apply(_Meta, {add, Key}, State) ->
+    sbf:add(Key, State),
+    {State, ok, []};
+apply(_Meta, {check, Key}, State) ->
+    Reply = sbf:member(Key, State),
+    {State, Reply, []}.
+
 
 
 
